5e1ce01a9e81fa8e56819fd709e5be8036d2f1a3
csfree87 <csfree@icloud.com>
2022-05-03 08:08:15 +0000

    deployment to heroku

--- booking/urls.py
+++ booking/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('menu/', views.menu, name='menu'),
     path('bookings/', views.bookings, name='bookings'),
-
+    path('booking_detail/', views.detail, name='booking_detail'),
+    path('booking_form/', views.form, name='booking_form'),
+    path('booking_list/', views.list, name='booking_list'),
 
 ]
--- booking/views.py
+++ booking/views.py
@@ -11,3 +11,15 @@ def menu(request):
 
 def bookings(request):
     return render(request, 'bookings.html')
+
+
+def detail(request):
+    return render(request, 'booking_detail.html')
+
+
+def form(request):
+    return render(request, 'booking_form.html')
+
+
+def list(request):
+    return render(request, 'booking_list.html')
