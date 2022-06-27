a2450310c1c4ef54a0b340c79a87f112a8b2cc0c
csfree87 <csfree@icloud.com>
2022-06-27 20:01:09 +0000

    change background image to menu page, added further styling changes and positioning adjustments

--- booking/models.py
+++ booking/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class GuestInfo(models.Model):
     BookingName = models.CharField(max_length=100,help_text='Probably the surname of your party.',blank=True)
-    Guests = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(6)],default='1',help_text='Due to current government reulations this has a maximum value of 6.',blank=True)
+    Guests = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(6)],default='1',blank=True)
     Email = models.EmailField(max_length=254,help_text='Please provide email for confirmation.')
     Day = models.DateField(null=False, blank=False)
 
